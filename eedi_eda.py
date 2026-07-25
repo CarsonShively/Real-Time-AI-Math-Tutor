514c01e1931a4cfaffd34ef332b43011f6ffc45d
Carson Shively <carsonshively24@gmail.com>
2026-07-25 05:10:06 -0700

    backup

--- eedi_eda.py
+++ eedi_eda.py
@@ -3,7 +3,7 @@ from datasets import load_dataset
 def eedi_eda():
     dataset = load_dataset("Eedi/Question-Anchored-Tutoring-Dialogues-2k", "anchored-dialogues")
     
-    print(type(dataset["train"]))
+    print(dataset["train"][0].keys())
     
 if __name__ == "__main__":
     eedi_eda()
