6e129633b008661d004639dec154d99f6a376f82
Carson Shively <carsonshively24@gmail.com>
2026-07-25 05:05:51 -0700

    backup

--- build_samples.py
+++ build_samples.py
@@ -5,6 +5,7 @@ def build_samples():
     
     train = {}
     
+    train_index = 0
     for train_sample in dataset["train"]:
         problem = train_sample["question"]
         user_work = train_sample["student_incorrect_solution"]
@@ -13,7 +14,19 @@ def build_samples():
         
         conversation_turns = train_sample["conversation"].split("")
         
-        conversation = 
-        target = 
+        conversation = None
+        target = conversation_turns[0]
         
-        sample = {}
+        sample = {
+            "problem": problem,
+            "user_work": user_work,
+            "correct_work": correct_work,
+            "mistake": mistake,
+            "conversation": conversation,
+            "target": target
+        }
+        
+        train[train_index] = sample
+        train_index += 1
+        
+        
---
+++ eedi_eda.py
@@ -0,0 +1,9 @@
+from datasets import load_dataset
+
+def eedi_eda():
+    dataset = load_dataset("Eedi/Question-Anchored-Tutoring-Dialogues-2k", "anchored-dialogues")
+    
+    print(dataset.keys())
+    
+if __name__ == "__main__":
+    eedi_eda()
