4be107225933f36df414a449a426966ee0f516a2
Carson Shively <carsonshively24@gmail.com>
2026-07-25 22:48:46 -0700

    backup

--- mathdial_eda.py
+++ mathdial_eda.py
@@ -7,8 +7,7 @@ def mathdial_eda():
     print(dataset.keys())
     print(type(dataset["train"]))
     print(dataset["train"][0].keys())
-    print(dataset["train"][3]["conversation"])
-    print(type(dataset["train"][3]["conversation"]))
+
     
 if __name__ == "__main__":
     mathdial_eda()
