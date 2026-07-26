e637bb2e4d4968c7c32a32ec09907dd94afe3225
Carson Shively <carsonshively24@gmail.com>
2026-07-26 05:12:23 -0700

    backup

--- fine_tune.py
+++ fine_tune.py
@@ -3,6 +3,7 @@ from huggingface_hub import snapshot_download
 from pathlib import Path
 import json
 from sft import SFTConfig, SFTTrainer
+from peft import LoraConfig
 
 def fine_tune():
     model_name = "Qwen/Qwen3-4B-Instruct-2507"
@@ -30,8 +31,34 @@ def fine_tune():
     for sample in test:
         test_structured.append({"messages": sample})
     
+    out_path = Path("/kaggle/working/model")
+
+    lora_config = LoraConfig(
+        task_type="CASUAL_LM",
+        r=16,
+        lora_alpha=32,
+        lora_dropout=0.05,
+        bias="none",
+        target_modules=[
+            "q_proj",
+            "k_proj",
+            "v_proj",
+            "o_proj",
+            "up_proj",
+            "gate_proj",
+            "down_proj"
+        ]
+    )
 
     fine_tuning_config = SFTConfig(
-        
+        num_train_epochs=3,
+        per_device_train_batch_size=1,
+        per_device_eval_batch_size=1,
+        gradient_accumulation_steps=16,
+        learning_rate=2e-4,
+        weight_decay=0.01,
+        max_length=2048,
+        output_dir=out_path,
+        assistant_only_loss=True
     )
     
--- samples_eda.py
+++ samples_eda.py
@@ -1,6 +1,7 @@
 from huggingface_hub import snapshot_download
 from pathlib import Path
 import json
+from transformers import AutoTokenizer
 
 def samples_eda():
     samples = Path(snapshot_download(
@@ -9,15 +10,47 @@ def samples_eda():
         allow_patterns=["train.json", "test.json"]
     ))
     
+    model_name = "Qwen/Qwen3-4B-Instruct-2507"
+    
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    
     with open(samples / "train.json", "r") as con:
         train = json.load(con)
         
     with open(samples / "test.json", "r") as con:
         test = json.load(con)
     
-    print(train[26])
-    print("\n\n\n")
-    print(test[26])
+    train_structured = []
+    
+    for sample in train:
+        train_structured.append({"messages": sample})
+    
+    test_structured = []
+    
+    for sample in test:
+        test_structured.append({"messages": sample})
+        
+    
+    train_count = 0
+    train_max_len = 0
+    for sample in train_structured:
+        text = tokenizer.apply_chat_template(sample["messages"], tokenize=True, add_generation_prompt=False)
+        if len(text["input_ids"]) > 2048:
+            train_count += 1
+        train_max_len = max(train_max_len, len(text["input_ids"]))
+    print(f"{train_count} exceed")
+    print(f"train max: {train_max_len}")
+    
+    test_count = 0
+    test_max_len = 0
+    for sample in test_structured:
+        text = tokenizer.apply_chat_template(sample["messages"], tokenize=True, add_generation_prompt=False)
+        if len(text["input_ids"]) > 2048:
+            test_count += 1
+        test_max_len = max(test_max_len, len(text["input_ids"]))
+    
+    print(f"{test_count} exceed")
+    print(f"test max: {test_max_len}")
     
 if __name__ == "__main__":
     samples_eda()
