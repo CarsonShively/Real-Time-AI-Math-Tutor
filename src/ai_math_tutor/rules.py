4088a14e62627d460533d9791d6cde7e41ec736d
Carson Shively <carsonshively24@gmail.com>
2026-07-30 08:03:48 -0700

    backup

--- src/ai_math_tutor/rules.py
+++ src/ai_math_tutor/rules.py
@@ -3,10 +3,10 @@ Convert what you see in the image to a document, one to one recreation of what w
 """
         
 REASONING_RULES = r"""
-Use all of the context you have available to answer the students correctly. Write your answer as a note to a tutor not directly to the student, include 1 latex formated aid seperate from the note. Format the output in thte following JSON: {"note": "", "tutoring_aid": ""}
+Use all of the context you have available to answer the students questtion correctly. Write your answer as a note to the tutor not directly to the student, and include 1 latex formated aid. Format the output in the following JSON: {"note": "", "tutoring_aid": ""}
 """
 
 TUTOR_RULES = r"""
-Convert the reasoning note and tutoring aid into a tutor like response not just giving answers. The response should be enttierly spoken words, numbers spelled out as words etc, inteded for speech.
+Convert the reasoning note and tutoring aid into a tutor like response not just giving answers. The response should be enttierly spoken words, numbers spelled out as words etc, inteded for speech. Do not use any lattex format or symbols in this response at all. 
 """
 
